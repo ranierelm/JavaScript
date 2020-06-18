62342196b0e85a8a1770225fcdb88464af48b731
ranierelm <raniereprojeto@gmail.com>
2020-06-17 21:40:30 -0300

    ex008 - IF

---
+++ CursoemVideo/Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 80.5
+
+console.log(`A velocidade do seu carro é:  ${vel}km/h`)
+if(vel>80){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto`)
