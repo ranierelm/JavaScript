9ca20c6d1e371fcedd928a389bbab8eab0e4a1ec
ranierelm <raniereprojeto@gmail.com>
2020-07-01 19:59:13 -0300

    Concluindo contador

--- CursoemVideo/Aula14/ex016/script.js
+++ CursoemVideo/Aula14/ex016/script.js
@@ -5,20 +5,24 @@ function contar(){
     let res = document.getElementById('res')
     
     if (inicio.value.length  == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = "Impossível contar!"
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: <br>'
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         
         if(i < f){
-            for(var c = i ; c<=f;c+=p){
-                res.innerHTML += `${c}, `
+            for(var c = i ; c <= f ; c += p){
+                res.innerHTML += ` \u{1F449} ${c}`
             }
-        } else {
-            for(var c = f ; c>=i;c-=p){
-                res.innerHTML += `${c}, `
+        } else if (i > f){
+            for(var c = f ; c >= i ; c -= p ){
+                res.innerHTML += `\u{1F449} ${c} `
             }
         }
         
