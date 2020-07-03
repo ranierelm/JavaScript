44383970b84f04bd6f56d17781a5bc4ec531edd4
ranierelm <raniereprojeto@gmail.com>
2020-07-02 22:04:44 -0300

    Funções compostas

---
+++ Aula 16/ex018d.js
@@ -0,0 +1,21 @@
+function fatorial(n){
+    let fat = 1
+    for(let c = n ; c > 1 ; c--){
+        fat *= c
+    }
+    return fat
+}
+console.log(fatorial(12))
+
+/* RECURSIVIDADE
+
+function fatorial(n){
+    if (n == 1){
+        return 1
+    } else {
+        return n* fatorial(n-1)
+    }
+}
+console.log(fatorial(5))
+
+*/
